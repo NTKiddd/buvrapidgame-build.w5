531def7d3ecb84dd908517ecc083fcaa20fc1863
NTKiddd <ngotrankhanh163@gmail.com>
2022-10-23 11:41:38 +0700

    Tasksheet 5 MM completed

---
+++ BUVRapidGamePrototyping/Assets/build.w1/Scripts/DisplayGameOver.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayGameOver : MonoBehaviour
+{
+    private GameObject gameOverDisplay;
+    public GameObject crosshairDisplay;
+    private Timer timerScript;
+    public Text gameOverText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOverDisplay = GameObject.Find("_TimerManager");
+        timerScript = gameOverDisplay.GetComponent<Timer>();
+        crosshairDisplay = GameObject.Find("crosshairUI");
+        Color transparent = new Color(0f, 0f, 0f, 0f);
+        gameOverText.color = transparent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerScript.timerClock == 0)
+        {    
+            gameOverText.text = "Game Over";
+            gameOverText.color = Color.red;
+            crosshairDisplay.GetComponent<RawImage> ().enabled=false;
+        }
+    }
+}
